6e38daab4651df177bde4d1c1d288dafef3bb88b
Jamie Norrish <jamie@artefact.org.nz>
2017-11-25 13:50:56 +1300

    Changed slider JS to use form input name values rather than ids. Added function comments.

--- webapps/ROOT/assets/scripts/slider.js
+++ webapps/ROOT/assets/scripts/slider.js
@@ -1,16 +1,29 @@
 /**
-/* Code for handling the creation and updating of a range slider for
-/* use in forms.
-/*
-/* This code depends on jQuery, jQuery UI, and URI.js.
-**/
+ * Code for handling the creation and updating of a range slider for
+ * use in forms.
+ *
+ * This code depends on jQuery, jQuery UI, and URI.js.
+ */
 
+/**
+ * Updates the label of a slider.
+ *
+ * @param widget - The HTML element of the slider widget.
+ * @param label - The HTML element of the slider label.
+ * @param values - Array of two values to be put in the label.
+ */
 function update_slider_label(widget, label, values) {
     var prefix = widget.data("label-prefix") + " ",
         suffix = " " + widget.data("label-suffix");
     label.text(prefix + values[0] + " – " + values[1] + suffix);
 }
 
+/**
+ * Creates the slider.
+ *
+ * @param widget - The HTML element to be used for the slider widget.
+ * @param label - The HTML element to be used for the slider label.
+ */
 function setup_slider(widget, label) {
     widget.slider({
         range: true,
@@ -38,7 +51,17 @@ function setup_slider(widget, label) {
     });
 }
 
-function prepare_form(form, slider, inputs) {
+/**
+ * Handles form submission and setting the slider values.
+ *
+ * @param form - The HTML form element.
+ * @param slider - The HTML element of the slider widget.
+ * @param inputs - Array of input/@name values for inputs that are not
+ *                 radio buttons or checkboxes.
+ * @param checked_inputs - Array of input/@name values for radio
+ *                         buttons and checkboxes.
+ */
+function prepare_form(form, slider, inputs, checked_inputs) {
     var field_name = slider.data("field-name");
     var params = URI.parseQuery(URI.parse(document.location.href).query);
     if (params[field_name + "_start"]) {
@@ -51,8 +74,12 @@ function prepare_form(form, slider, inputs) {
     form.on("submit", function(e) {
         e.preventDefault();
         for (i = 0; i < inputs.length; i++) {
-            params[inputs[i]] = $("#" + inputs[i]).val();
+            params[inputs[i]] = $("*[name=" + inputs[i] + ']').val();
         }
+        for (i = 0; i < checked_inputs.length; i++) {
+            values = $("*[name=" + checked_inputs[i] + ']').map(
+                function() { return $(this).val(); });
+            params[checked_inputs[i]] = values;
         document.location.href = "?" + URI.buildQuery(params);
     });
 }
